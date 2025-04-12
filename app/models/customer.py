8c5d6a50b41d80b572ad4d174828913f72369e35
ReginaldCosensIII <ReginaldCosensIII@gmail.com>
2025-04-12 12:34:29 -0400

    Create customer.py
    Adding new files/structure for the Bookstore App

---
+++ app/models/customer.py
@@ -0,0 +1,24 @@
+# app/models/customer.py
+from flask_login import UserMixin
+
+class Customer(UserMixin):
+    def __init__(self, id, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+
+    # Flask-Login integration
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
