4c81bdf5b245f3249a393bfb0a26697be13b9515
ReginaldCosensIII <ReginaldCosensIII@gmail.com>
2025-04-25 22:04:40 -0400

    Update main.py
    Change the structure into production from debug mode

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-# main.py
 from app import create_app
 from logger import logger  # Custom logger
 
+# Create the Flask application
 app = create_app()
 
 if __name__ == '__main__':
-    # Run the Flask application
-    # with debug mode enabled for development.
+    # This will only run in development, Gunicorn will handle in production
     logger.info("Starting the Flask application...")
-    app.run(debug=True)
+    app.run(debug=True)  # Set debug=True only for local testing
     logger.info("Flask application has stopped.")
-    # Note: In production, you would typically use a WSGI server like Gunicorn or uWSGI.
-    # The debug=True option is not recommended for production use.
